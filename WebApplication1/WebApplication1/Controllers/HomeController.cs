2f9372c688c12db8aea2b9779070b556230bed38
Luan <thanhLUAN-png@users.noreply.github.com>
2026-08-12 13:48:32 +0700

    Update Home page UI: Redesign UI to match Figma with single wave at bottom and live stats

--- WebApplication1/WebApplication1/Controllers/HomeController.cs
+++ WebApplication1/WebApplication1/Controllers/HomeController.cs
@@ -1,13 +1,20 @@
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
+using WebApplication1.Data;
 using WebApplication1.Models;
 
 namespace WebApplication1.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly OceanDbContext _db;
+        public HomeController(OceanDbContext db) => _db = db;
+
         public IActionResult Index()
         {
+            ViewBag.ApprovedCount  = _db.ArtworkSubmissions.Count(a => a.Status == "Approved");
+            ViewBag.TemplateCount  = _db.CreatureTemplates.Count(t => t.IsActive);
+            ViewBag.ArtistCount    = _db.Users.Count();
             return View();
         }
 
